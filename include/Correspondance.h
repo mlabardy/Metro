ee147f67a635c9c5a7cb5e05a4d1abea2a633836
mlabardy <mlabardy@etudiant.univ-mlv.fr>
2013-05-24 21:29:00 +0300

    Update Correspondance.h

--- include/Correspondance.h
+++ include/Correspondance.h
@@ -17,19 +17,22 @@ Correspondance *creer_Correspondance(int nbLignes, int nbStations);
 /* Cette fonction désalloue la mémoire allouée pour une correspondance. */
 void liberer_Correspondance(Correspondance *c);
 
-/*  */
+/* Cette fonction permet d'ajouter une ligne de métro dans le tableau de lignes. */
 void ajouter_Ligne(char ligne, char *lignes, int nbLignes);
 
+/* Cette fonction ajoute le temps de changement de lignes entre deux lignes. */
 void ajouter_Correspondance(Correspondance *c, char origine, char destination, int tempsCorresp, char *lignes);
 
-void ecrire_association_Correspondance(int nbLignes, char *lignes, FILE *sortie);
-
+/* Cette fonction affiche sur la sortie une correspondance. */
 void ecrire_Correspondance(Correspondance *c, FILE *sortie);
 
+/* Cette fonction lit une correspondance depuis une entrée, et stocke ces informations dans la structure Correspondance. */
 int lire_Correspondance(Correspondance **c, FILE *entree, char ligneD, char *lignes, int nbLignes);
 
+/* Cette fonction désalloue la mémoire allouée pour une correspondance. */
 void liberer_Tab_Correspondance(Correspondance **c, int taille);
 
+/* Cette fonction crée un tableau de correspondances. */
 Correspondance **creer_Tab_Correspondance(int taille);
 
 #endif
